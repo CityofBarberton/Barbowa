ff471881759102df45a00b5cd67849a38ee2ccfc
Christopher Stoll <stollcri@gmail.com>
2014-01-31 12:04:37 -0500

    Spaces to tabs

--- rest.go
+++ rest.go
@@ -1,22 +1,22 @@
 package main
 
 import (
-    "net/url"
-    "github.com/dougblack/sleepy"
+	"net/url"
+	"github.com/dougblack/sleepy"
 )
 
 type Contact struct { }
 
 func (contact Contact) Get(values url.Values) (int, interface{}) {
-    contacts := []string{"contact1", "contact2"}
-    data := map[string][]string{"contacts": contacts}
-    return 200, data
+	contacts := []string{"contact1", "contact2"}
+	data := map[string][]string{"contacts": contacts}
+	return 200, data
 }
 
 func main() {
-    contact := new(Contact)
+	contact := new(Contact)
 
-    var api = sleepy.NewAPI()
-    api.AddResource(contact, "/contacts")
-    api.Start(8080)
+	var api = sleepy.NewAPI()
+	api.AddResource(contact, "/contacts")
+	api.Start(8080)
 }
