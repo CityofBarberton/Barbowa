1aaa5e63a57603d30eb694bf43eeef7c6e90b18a
Christopher Stoll <stollcri@gmail.com>
2014-01-31 12:02:27 -0500

    Stub in facade for getting contact data

---
+++ contact.go
@@ -0,0 +1,16 @@
+/*
+Get contact data
+
+This is a facade for all of the things required to get a list of contacts
+ - department
+ - position
+ - person
+ - place
+*/
+package main
+
+import "contact-static"
+
+func getContacts() {
+	//
+}
